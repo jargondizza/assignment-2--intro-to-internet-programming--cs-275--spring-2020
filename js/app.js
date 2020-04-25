0de9af8302a989b1696de25db2a93d6c09fde190
jargondizza <argondizz@hartford.edu>
2020-04-24 23:50:43 -0400

    Add EventListener to getRecipe

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
+const x = document.getElementById(`getRecipe`);
+
+x.addEventListener(`click`, on_change);
+
 function on_change(el) {
-    if (el.options[el.selectedIndex].value == 'one') {
-        document.getElementById('rice1').style.display = 'block';
+    if (el.options[el.selectedIndex].value == `one`) {
+        document.getElementById(`rice1`).style.display = `block`;
     } else {
-        document.getElementById('rice1').style.display = 'none';
+        document.getElementById(`rice1`).style.display = `none`;
     }
-    if (el.options[el.selectedIndex].value == 'two') {
-        document.getElementById('rice2').style.display = 'block';
+    if (el.options[el.selectedIndex].value == `two`) {
+        document.getElementById(`rice2`).style.display = `block`;
     } else {
-        document.getElementById('rice2').style.display = 'none';
+        document.getElementById(`rice2`).style.display = `none`;
     }
 }
