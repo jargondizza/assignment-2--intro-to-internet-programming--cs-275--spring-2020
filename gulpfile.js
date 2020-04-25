7c1832f869e5c1a2670c97f39dc9eba747cdac60
jargondizza <argondizz@hartford.edu>
2020-04-24 20:36:35 -0400

    Add ./temp to baseDir

--- gulpfile.js
+++ gulpfile.js
@@ -19,10 +19,12 @@ let serve = () => {
         reloadDelay: 0, // A delay is sometimes helpful when reloading at the
         server: {       // end of a series of tasks.
             baseDir: [
+                `./temp`,
                 `html`
             ]
         }
     });
+    watch(`html/**/*.html`).on(`change`, reload);
 };
 
 let lintJS = () => {
