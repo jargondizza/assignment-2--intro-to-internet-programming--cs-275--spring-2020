57dc9c3dfe363ac9e380155448cbd2dd3cc0c49e
jargondizza <argondizz@hartford.edu>
2020-04-29 12:17:41 -0400

    Add const x to the function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const x = document.getElementById(`getRecipe`);
 x.addEventListener(`click`, on_change);
 
-function on_change(el) {
-    if (el.options[el.selectedIndex].value == `one`) {
+function on_change() {
+    if (x.options[x.selectedIndex].value == `one`) {
         document.getElementById(`rice1`).style.display = `block`;
     } else {
         document.getElementById(`rice1`).style.display = `none`;
     }
 
-    if (el.options[el.selectedIndex].value == `two`) {
+    if (x.options[x.selectedIndex].value == `two`) {
         document.getElementById(`rice2`).style.display = `block`;
     } else {
         document.getElementById(`rice2`).style.display = `none`;
